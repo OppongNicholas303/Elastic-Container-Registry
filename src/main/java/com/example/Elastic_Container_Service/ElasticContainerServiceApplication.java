506aa6e855f003e33d70f2c689022cc7141e997e
nicholas <nicholas.oppong@amalitech.com>
2025-03-06 16:59:22 +0000

    Initial commit

---
+++ src/main/java/com/example/Elastic_Container_Service/ElasticContainerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Elastic_Container_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticContainerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticContainerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Elastic_Container_Service/ElasticContainerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Elastic_Container_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticContainerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
